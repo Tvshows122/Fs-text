91c0f39d843062e46a251c181ce4a221c74fc5e1
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2023-10-30 16:18:09 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("TG - whiteDevilBots")
+    return web.json_response("TG - bhavy")
